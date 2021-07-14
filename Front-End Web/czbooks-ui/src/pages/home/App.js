07a0b5f9d3e0e6eb74cdce9e16743a1bc8004213
Rafael Porto <R4f4el_Porto@hotmail.com>
2021-07-14 10:51:43 -0300

    adc

--- Front-End Web/czbooks-ui/src/pages/home/App.js
+++ Front-End Web/czbooks-ui/src/pages/home/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import { Link } from 'react-router-dom';
+
+import '../../assets/css/App';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
+    <div>
+      <header>
+        <div>
+          <Link to="/"></Link>
+          <h1>colocar logo aq em cima</h1>
+
+          <nav>
+            <Link to="/">Home</Link>  
+            <Link to="cliente">Livros</Link>  
+            <Link to="autor">Autor</Link>  
+            <Link to="adm">Adm</Link>  
+            <Link to="login">Login</Link>  
+          </nav>
+        </div>
       </header>
+
+
     </div>
   );
 }
